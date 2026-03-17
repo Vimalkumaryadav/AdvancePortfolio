ddd7a2ee8d90a66433a8cd39bea07d94d85eeafc
Vimal Kumar Yadav <yadavvimalk@gmail.com>
2026-03-18 04:20:02 +0530

    Updates

--- assets/Navbar-CP4UWmjB.js
+++ assets/Navbar-CP4UWmjB.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-0SSbh-XT.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-BWSQ5pWs.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
